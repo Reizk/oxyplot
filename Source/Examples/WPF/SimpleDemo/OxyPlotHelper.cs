9e7bdbfd1ea314ea0a63200068e4406fda3f097d
zhang.yichi <zhang.yichi@SHR117-ZHANGYCH>
2020-08-20 09:35:14 +0800

    update Contextmenu

--- Source/Examples/WPF/SimpleDemo/OxyPlotHelper.cs
+++ Source/Examples/WPF/SimpleDemo/OxyPlotHelper.cs
@@ -1,9 +1,11 @@
 ﻿using OxyPlot;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace SimpleDemo
 {
@@ -26,18 +28,41 @@ namespace SimpleDemo
 
         private static void IsCheckedPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d != null && d is MenuItem)
-            {
-                var mi = d as MenuItem;
+            //if (d != null && d is MenuItem)
+            //{
+            //    var mi = d as MenuItem;
 
-                if (mi.Tag != null && mi.Tag is PlotModel)
-                {
-                    var plotModel = mi.Tag as PlotModel;
-                    plotModel.InvalidatePlot(false);
-                    plotModel.ResetAllAxes();
-                }
+            //    if (mi.Tag != null && mi.Tag is PlotModel)
+            //    {
+            //        var plotModel = mi.Tag as PlotModel;
+            //        plotModel.InvalidatePlot(false);
+            //        plotModel.ResetAllAxes();
+            //    }
 
-            }
+            //}
         }
     }
+
+
+
+    public class OxyPlotInvalidateConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (values[1] != null && values[1] is PlotModel)
+            {
+                var plotModel = values[1] as PlotModel;
+                plotModel.InvalidatePlot(false);
+                plotModel.ResetAllAxes();
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new object[] { Binding.DoNothing, Binding.DoNothing };
+        }
+    }
+
 }
